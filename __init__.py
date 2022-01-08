de70c2a2cc7529a3ec860f485876f5746a2be606
Pedro Bonfilio <pedroblimaa@gmail.com>
2022-01-08 17:52:09 -0300

    Add deletion of files when already exist

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
 import filesManager as fm
 import os
 
+
 def main():
     path = "C:\\Users\\Pedro\\Downloads"
     files = os.listdir(path)
-    fm.createFolders(files)
+    fm.createFolders(files, path)
     fm.moveFiles(files, path)
+
 
 if __name__ == "__main__":
     main()
--- filesManager.py
+++ filesManager.py
@@ -1,23 +1,28 @@
 import consts as c
 import os
 
-def createFolders(files):
+
+def createFolders(files, path):
     for folder in c.folders:
         if not folder in files:
-            os.mkdir("C:\\Users\\Pedro\\Downloads\\" + folder)
+            os.mkdir(path + "\\" + folder)
+
 
 def moveFiles(files, path):
     print("Moving files...")
     for file in files:
         for folder in c.folders:
             if file.endswith(c.foldersName[folder]):
+                filePath = path + "\\" + folder + "\\" + file
+                if os.path.exists(filePath):
+                    os.remove(filePath)
                 os.rename(
                     path + "\\" + file,
-                    "C:\\Users\\Pedro\\Downloads\\" + folder + "\\" + file,
+                    path + "\\" + folder + "\\" + file,
                 )
         if os.path.isdir(path + "\\" + file) and (not file in c.folders):
             print("Folder: " + file)
             os.rename(
                 path + "\\" + file,
-                "C:\\Users\\Pedro\\Downloads\\other\\" + file,
+                path + "\\other\\" + file,
             )
