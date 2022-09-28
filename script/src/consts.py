9f8bbda4b00c5a3dcc73a5c71f6c6efeb2b6e624
Pedro <pedroblimaa@gmail.com>
2022-09-27 21:17:45 -0300

    Fix media

--- script/src/consts.py
+++ script/src/consts.py
@@ -1,18 +1,17 @@
 documents = (".doc", ".docx", ".txt", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx")
-media = (".mp4", ".avi", ".mp3", ".mkv", ".flv", ".wmv", ".mov",
-         ".mpg", ".mpeg", ".jpeg", ".jpg", ".png", ".gif", ".ico")
+media = (".mp4", ".avi", ".mp3", ".mkv", ".flv", ".wmv", ".mov", ".mpg", ".mpeg", ".jpeg", ".jpg", ".png", ".gif", ".ico")
 compressed = (".zip", ".rar", ".7z")
 installer = (".exe", ".msi")
 other = ()
 languages = ("pt-br", "en-us")
 
-foldersName = [
-    documents,
-    media,
-    compressed,
-    installer,
-    other
-]
+foldersName = {
+    "documents": documents,
+    "media": media,
+    "compressed": compressed,
+    "installer": installer,
+    "other": other,
+}
 
 foldersLanguages = (
     {
@@ -21,7 +20,7 @@ foldersLanguages = (
     },
     {
         "language": 'pt-br',
-        "names": ("Documentos", "Media", "Zips", "Instaladores", "Outros")
+        "names": ("Documentos", "Midia", "Zips", "Instaladores", "Outros")
     },
     {
         "language": 'en-us',
